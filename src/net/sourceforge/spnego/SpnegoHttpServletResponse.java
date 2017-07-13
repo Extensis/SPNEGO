f93b3074b553707187bbd7f89f3b8e993201eb65
Brooks Brown <bbrown@extensis.com>
2017-07-13 16:59:19 -0700

    Changed SpnegoHttpServletResponse to non-final so it can be extended. In my client code, I overrode setStatus(int, boolean) to send message content so that when a client isn’t able to authenticate, they get our app with the login form displayed.

--- src/net/sourceforge/spnego/SpnegoHttpServletResponse.java
+++ src/net/sourceforge/spnego/SpnegoHttpServletResponse.java
@@ -1,16 +1,16 @@
-/** 
+/**
  * Copyright (C) 2009 "Darwin V. Felix" <darwinfelix@users.sourceforge.net>
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
@@ -24,24 +24,24 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
 /**
- * Class adds capability to track/determine if the HTTP Status 
+ * Class adds capability to track/determine if the HTTP Status
  * code has been set.
- * 
+ *
  * <p>
- * Also allows the ability to set the content length to zero 
- * and flush the buffer immediately after setting the HTTP 
+ * Also allows the ability to set the content length to zero
+ * and flush the buffer immediately after setting the HTTP
  * Status code.
  * </p>
- * 
+ *
  * @author Darwin V. Felix
- * 
+ *
  */
-public final class SpnegoHttpServletResponse extends HttpServletResponseWrapper  {
+public class SpnegoHttpServletResponse extends HttpServletResponseWrapper  {
 
     private transient boolean statusSet = false;
 
     /**
-     * 
+     *
      * @param response
      */
     public SpnegoHttpServletResponse(final HttpServletResponse response) {
@@ -50,7 +50,7 @@ public final class SpnegoHttpServletResponse extends HttpServletResponseWrapper 
 
     /**
      * Tells if setStatus has been called.
-     * 
+     *
      * @return true if HTTP Status code has been set
      */
     public boolean isStatusSet() {
@@ -64,13 +64,13 @@ public final class SpnegoHttpServletResponse extends HttpServletResponseWrapper 
     }
 
     /**
-     * Sets the HTTP Status Code and optionally set the the content 
+     * Sets the HTTP Status Code and optionally set the the content
      * length to zero and flush the buffer.
-     * 
+     *
      * @param status http status code
      * @param immediate set to true to set content len to zero and flush
-     * @throws IOException 
-     * 
+     * @throws IOException
+     *
      * @see #setStatus(int)
      */
     public void setStatus(final int status, final boolean immediate) throws IOException {
